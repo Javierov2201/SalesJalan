37bf85593f1b74f78102b06c89e938967a971619
Mohamad Fachrur Ridwan <ridwan.fachrur@gmail.com>
2016-12-01 12:43:42 +0700

    city package to hold the amounts of cities

--- SalesJalanVisualize/src/salesjalanvisualize/city/Kota.java
+++ SalesJalanVisualize/src/salesjalanvisualize/city/Kota.java
@@ -5,10 +5,37 @@
  */
 package salesjalanvisualize.city;
 
+import javafx.geometry.Point2D;
+
 /**
  *
  * @author rfachrur
  */
-public class Kota {
+public class Kota implements Comparable<Kota> {
     
+    private final Point2D location;
+    
+    public Kota(double x, double y) {
+        this.location = new Point2D(x, y);
+    }
+
+    @Override
+    public int compareTo(Kota o) {
+        return (int) this.getLocation().distance(o.getLocation());
+    }
+
+    public int distanceTo(Kota o) {
+        return (int) this.getLocation().distance(o.getLocation());
+    }
+
+    // get location in 2D points form
+    public Point2D getLocation() {
+        return location;
+    }
+
+    @Override
+    public String toString() {
+        return location.toString();
+    }
+
 }
---
+++ SalesJalanVisualize/src/salesjalanvisualize/city/ManageKota.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salesjalanvisualize.city;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rfachrur
+ */
+public class ManageKota {
+    
+    private static final ManageKota instance = new ManageKota();
+    private final ArrayList<Kota> cities;
+    
+     public static ManageKota getInstance() {
+        return instance;
+    }
+
+    private ManageKota() {
+        cities = new ArrayList<>();
+    }
+    // Holds the cities
+
+    // To add the cities destination
+    public void addKota(Kota kota) {
+        cities.add(kota);
+    }
+
+    // Getting the city
+    public Kota getCity(int index) {
+        return cities.get(index);
+    }
+
+    // Get the number of destination cities
+    public int numberOfCities() {
+        return cities.size();
+    }
+    
+}
